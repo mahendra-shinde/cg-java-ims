2a0be36e1a9bb8ee70994c7eceeb8478c9455d24
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2018-08-25 12:08:25 +0530

    SOAP WebServices

---
+++ WebServices/Services/src/com/mahendra/CalcService.java
@@ -0,0 +1,13 @@
+package com.mahendra;
+
+
+public class CalcService {
+
+	public Double doSum(Double n1, Double n2) {
+		return n1+n2;
+	}
+	
+	public Double doSub(Double n1,Double n2) {
+		return n1-n2;
+	}
+}
